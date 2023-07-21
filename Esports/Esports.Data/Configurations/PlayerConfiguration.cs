c802b0e136197c99a25bbe81389655c39f3e8bfe
Pigon <mramutta@gmail.com>
2023-07-21 13:37:16 +0300

    Added all teams and some players

--- Esports/Esports.Data/Configurations/PlayerConfiguration.cs
+++ Esports/Esports.Data/Configurations/PlayerConfiguration.cs
@@ -1,4 +1,5 @@
-﻿using Esports.Data.Models;
+﻿using Esports.Data.Helpers;
+using Esports.Data.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using System;
@@ -18,7 +19,71 @@ namespace Esports.Data.Configurations
                 .WithMany(t => t.Players)
                 .OnDelete(DeleteBehavior.Restrict);
 
+            builder.HasData(GeneratePlayers());
+        }
 
+        public List<Players> GeneratePlayers()
+        {
+            List<Players> players = new List<Players>();
+            ImageConverter imageConverter = new ImageConverter();
+
+            players.Add(new Players()
+            {
+                Id = Guid.Parse("f19b3751-9016-4c69-86e6-683eef3baa37"),
+                Name = "Óscar Muñoz Jiménez",
+                Age = 20,
+                Nationality = "Spanish",
+                Position = "top",
+                Image = imageConverter.ConvertToByteArray("..\\Esports.Data\\Images\\lec\\fnatic\\FNC_Oscarinin_2023_Split_1.png"),
+                TeamId = Guid.Parse("F82A5401-0DF9-4219-A9C3-C963E1168DBF")
+            });
+
+            players.Add(new Players()
+            {
+                Id = Guid.Parse("9b0b95bf-a0b1-4eec-b439-1c86b4dca1e7"),
+                Name = "Iván Martín Díaz",
+                Age = 22,
+                Nationality = "Spanish",
+                Position = "jng",
+                Image = imageConverter.ConvertToByteArray("..\\Esports.Data\\Images\\lec\\fnatic\\FNC_Razork_2023_Split_1.png"),
+                TeamId = Guid.Parse("F82A5401-0DF9-4219-A9C3-C963E1168DBF")
+            });
+
+            players.Add(new Players()
+            {
+                Id = Guid.Parse("c622fec6-cd1c-4ec1-9d50-b60b3c937fe9"),
+                Name = "Marek Brázda",
+                Age = 23,
+                Nationality = "Czech",
+                Position = "mid",
+                Image = imageConverter.ConvertToByteArray("..\\Esports.Data\\Images\\lec\\fnatic\\FNC_Humanoid_2023_Split_1.png"),
+                TeamId = Guid.Parse("F82A5401-0DF9-4219-A9C3-C963E1168DBF")
+            });
+
+            players.Add(new Players()
+            {
+                Id = Guid.Parse("1fd70037-7642-4c28-a20a-68a44efb02af"),
+                Name = "Oh Hyeon-taek",
+                Age = 21,
+                Nationality = "Korean",
+                Position = "adc",
+                Image = imageConverter.ConvertToByteArray("..\\Esports.Data\\Images\\lec\\fnatic\\Z10_Noah_2023_Split_1.png"),
+                TeamId = Guid.Parse("F82A5401-0DF9-4219-A9C3-C963E1168DBF")
+            });
+
+            players.Add(new Players()
+            {
+                Id = Guid.Parse("adb1aecb-33dd-4679-9dc0-4f7faf93e823"),
+                Name = "Adrian Trybus",
+                Age = 22,
+                Nationality = "Polish",
+                Position = "sup",
+                Image = imageConverter.ConvertToByteArray("..\\Esports.Data\\Images\\lec\\fnatic\\KOI_Trymbi_2023_Split_1.png"),
+                TeamId = Guid.Parse("F82A5401-0DF9-4219-A9C3-C963E1168DBF")
+            });
+
+
+            return players;
         }
     }
 }
--- Esports/Esports.Data/Configurations/TeamConfiguration.cs
+++ Esports/Esports.Data/Configurations/TeamConfiguration.cs
@@ -28,7 +28,7 @@ namespace Esports.Data.Configurations
                 Name = "Fnatic",
                 CountryOrigin = "United Kingdom",
                 Titles = 7,
-                Logo = converter.ConvertToByteArray("..\\Esports.Data\\Images\\Fnaticlogo_square.png")
+                Logo = converter.ConvertToByteArray("..\\Esports.Data\\Images\\lec\\fnatic\\Fnaticlogo_square.png")
                 
             });
 
@@ -38,10 +38,89 @@ namespace Esports.Data.Configurations
                 Name = "G2",
                 CountryOrigin = "Spain",
                 Titles = 10,
-                Logo = converter.ConvertToByteArray("..\\Esports.Data\\Images\\G2_Esportslogo_profile.png")
+                Logo = converter.ConvertToByteArray("..\\Esports.Data\\Images\\lec\\g2\\G2_Esportslogo_square.png")
 
             });
 
+            teams.Add(new Teams
+            {
+                Id = Guid.Parse("ff55afe4-6156-4ce3-b848-a1d0a7472248"),
+                Name = "Mad Lions",
+                CountryOrigin = "Spain",
+                Titles = 3,
+                Logo = converter.ConvertToByteArray("..\\Esports.Data\\Images\\lec\\madlions\\MAD_Lionslogo_square.png" )
+
+            });
+
+            teams.Add(new Teams
+            {
+                Id = Guid.Parse("fea8fd74-d07e-4521-ad82-ba2fa2c270cb"),
+                Name = "KOI",
+                CountryOrigin = "Spain",
+                Titles = 0,
+                Logo = converter.ConvertToByteArray("..\\Esports.Data\\Images\\lec\\koi\\KOI_logo_square.png")
+
+            });
+
+            teams.Add(new Teams
+            {
+                Id = Guid.Parse("0452bb8c-4c13-400a-846e-81cfa0193185"),
+                Name = "Excel",
+                CountryOrigin = "United Kingdom",
+                Titles = 0,
+                Logo = converter.ConvertToByteArray("..\\Esports.Data\\Images\\lec\\excel\\Excel_Esportslogo_square.png")
+
+            });
+
+            teams.Add(new Teams
+            {
+                Id = Guid.Parse("647b6346-dd83-4cf2-aed0-222d9f104e5f"),
+                Name = "Astralis",
+                CountryOrigin = "Denmark",
+                Titles = 0,
+                Logo = converter.ConvertToByteArray("..\\Esports.Data\\Images\\lec\\astralis\\Astralislogo_square.png")
+
+            });
+
+            teams.Add(new Teams
+            {
+                Id = Guid.Parse("79fc23d1-4fca-4b75-a2f2-291ea4a2c777"),
+                Name = "Team BDS",
+                CountryOrigin = "Switzerland",
+                Titles = 0,
+                Logo = converter.ConvertToByteArray("..\\Esports.Data\\Images\\lec\\teambdsm\\Team_BDSlogo_square.png")
+
+            });
+
+            teams.Add(new Teams
+            {
+                Id = Guid.Parse("26011ad9-f7c3-4046-887c-2408cb5a4e92"),
+                Name = "SK Gaming",
+                CountryOrigin = "Germany",
+                Titles = 0,
+                Logo = converter.ConvertToByteArray("..\\Esports.Data\\Images\\lec\\skgaming\\SK_Gaminglogo_square.png")
+
+            });
+
+            teams.Add(new Teams
+            {
+                Id = Guid.Parse("9215d293-4a88-4da2-abfe-f5d4bf824602"),
+                Name = "Team Heretics",
+                CountryOrigin = "Spain",
+                Titles = 0,
+                Logo = converter.ConvertToByteArray("..\\Esports.Data\\Images\\lec\\teamheretics\\Team_Hereticslogo_square.png")
+
+            });
+
+            teams.Add(new Teams
+            {
+                Id = Guid.Parse("914c7452-ce34-4162-a7f4-c364234b17ea"),
+                Name = "Team Vitality",
+                CountryOrigin = "France",
+                Titles = 0,
+                Logo = converter.ConvertToByteArray("..\\Esports.Data\\Images\\lec\\teamvitality\\Team_Vitalitylogo_square.png")
+
+            });
 
             return teams;
         }
